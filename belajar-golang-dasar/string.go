c440c784d2ae60a53f7e237b57e1b47367ce4228
eskapdk <eureka.elwing@gmail.com>
2023-01-28 13:14:11 +0700

    komit pertama
    komit pertama materi belajar golang

---
+++ belajar-golang-dasar/string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Eska Pratama Dian Kurniawan")
+	fmt.Println("Ratih Nurmalasari")
+	fmt.Println(len("Eska Pratama Dian Kurniawan"))
+	fmt.Println("Ratih Nurmalasari"[2])
+}
---
+++ belajar-golang-dasar/variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Eska Pratama"
+	fmt.Println(name)
+
+	name = "Dian Kurniawan"
+	fmt.Println(name)
+
+	var friendName = "Andi"
+	fmt.Println(friendName)
+
+	var age = 29
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Eska"
+		lastName  = "Pratama"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
